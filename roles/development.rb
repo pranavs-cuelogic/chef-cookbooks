8b3995bb5d4c9ff27e5d37235ef0f75fb280ba5c
Chanakya Devraj <chanakya.devraj@cuelogic.com>
2015-01-16 11:32:18 +0530

    just rails now

--- roles/development.rb
+++ roles/development.rb
@@ -1,5 +1,5 @@
 name "development"
 description "A role to configure our front-line web servers"
 run_list  "recipe[yum]", "recipe[yum-epel]", "recipe[apt]", "recipe[build-essential]", "recipe[ohai]", "recipe[runit]", "recipe[cpu]", "recipe[install_from]", 
-"recipe[metachef]", "recipe[openssl]", "recipe[chef-sugar]", "recipe[memcached]", "recipe[redis]", "recipe[haproxy]", "recipe[postgresql]", "recipe[rails]" 
+"recipe[metachef]", "recipe[openssl]", "recipe[chef-sugar]", "recipe[rails]" 
 
